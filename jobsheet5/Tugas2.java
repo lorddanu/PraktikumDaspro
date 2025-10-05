fbe68ac4f7a2a8a46fe7a0c1f09ecdd39a79c638
lorddanu <ilhamdanu010@gmail.com>
2025-10-05 22:53:22 +0700

    Tugas 2

---
+++ jobsheet5/Tugas2.java
@@ -0,0 +1,21 @@
+package jobsheet5;
+
+import java.util.Scanner;
+
+public class Tugas2 {
+        public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int angka;
+
+        System.out.print("Masukkan angka: ");
+        angka = input.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Genap");
+        } else {
+            System.out.println("Ganjil");
+        }
+
+        input.close();
+    }
+}
