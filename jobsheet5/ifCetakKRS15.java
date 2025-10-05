40aaa669879bc2c9fe9450dedf2250b43bd5bfd1
lorddanu <ilhamdanu010@gmail.com>
2025-10-05 20:57:11 +0700

    Modifikasi Percobaan 1

--- jobsheet5/ifCetakKRS15.java
+++ jobsheet5/ifCetakKRS15.java
@@ -13,6 +13,8 @@ public class ifCetakKRS15 {
         if (uktlunas) {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+        } else {
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
         }
     }
 }
