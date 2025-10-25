264dc9b207462dc6507bbb3dcc237e46ae944422
lorddanu <ilhamdanu010@gmail.com>
2025-10-25 12:48:27 +0700

    percobaan ke 1

---
+++ jobsheet7/SiakadFor15.java
@@ -0,0 +1,23 @@
+package jobsheet7;
+import java.util.Scanner;
+
+public class SiakadFor15 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double nilai;
+        double tertinggi = 0, terendah = 100;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Masukkan nilai Mahasiswa ke-" + i + ": ");
+            nilai = sc.nextDouble();
+            if (nilai > tertinggi) {
+                tertinggi = nilai;
+            }
+            if (nilai < terendah) {
+                terendah = nilai;
+            }
+        }
+        System.out.println("Nilai tertinggi: " + tertinggi);
+        System.out.println("Nilai terendah: " + terendah);
+    }
+}
